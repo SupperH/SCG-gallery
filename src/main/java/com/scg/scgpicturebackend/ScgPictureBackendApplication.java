d4ff1559f8aa5d8ebb036ec863eb77130009824e
zeden <532334725@qq.com>
2025-05-01 15:13:05 +0800

    初始化项目

---
+++ src/main/java/com/scg/scgpicturebackend/ScgPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.scg.scgpicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScgPictureBackendApplication.class, args);
+    }
+
+}
